6899aa3566e6aaef3d2e2b43e3dcb33848f00062
ömer burak <116202251+Burakowsky@users.noreply.github.com>
2023-12-31 22:36:14 +0300

    This code defines a ReplicationLagChecker struct.

--- core/lib/circuit_breaker/src/replication_lag.rs
+++ core/lib/circuit_breaker/src/replication_lag.rs
@@ -2,15 +2,19 @@ use zksync_dal::ConnectionPool;
 
 use crate::{CircuitBreaker, CircuitBreakerError};
 
+// Struct for monitoring replication lag.
 #[derive(Debug)]
 pub struct ReplicationLagChecker {
     pub pool: ConnectionPool,
     pub replication_lag_limit_sec: Option<u32>,
 }
 
+// Implementing the CircuitBreaker trait for ReplicationLagChecker.
 #[async_trait::async_trait]
 impl CircuitBreaker for ReplicationLagChecker {
+    // Implementing the check method defined in the CircuitBreaker trait.
     async fn check(&self) -> Result<(), CircuitBreakerError> {
+        // Get the replication lag from the system and perform checks.
         let lag = self
             .pool
             .access_storage()
@@ -20,12 +24,17 @@ impl CircuitBreaker for ReplicationLagChecker {
             .get_replication_lag_sec()
             .await;
 
+        // Record replication lag in metrics.
         metrics::gauge!("circuit_breaker.replication_lag", lag as f64);
+
+        // Check if the replication lag exceeds the defined limit.
         match self.replication_lag_limit_sec {
-            Some(replication_lag_limit_sec) if lag > replication_lag_limit_sec => Err(
-                CircuitBreakerError::ReplicationLag(lag, replication_lag_limit_sec),
-            ),
+            Some(replication_lag_limit_sec) if lag > replication_lag_limit_sec => {
+                // Return an error if the replication lag exceeds the limit.
+                Err(CircuitBreakerError::ReplicationLag(lag, replication_lag_limit_sec))
+            }
             _ => Ok(()),
         }
     }
 }
+
