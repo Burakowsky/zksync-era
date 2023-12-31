8c0ebc42485c4e02489dbe0d1ebd48c2766d9ac2
ömer burak <116202251+Burakowsky@users.noreply.github.com>
2023-12-31 22:37:29 +0300

    This Rust code defines a function unwrap_tuple that takes a Token as input and attempts to unwrap it into a vector of Tokens. If the input Token is not a tuple, it panics with an error message displaying the unexpected token.

--- core/lib/circuit_breaker/src/utils.rs
+++ core/lib/circuit_breaker/src/utils.rs
@@ -1,9 +1,14 @@
+// Importing Token from zksync_types::ethabi module.
 use zksync_types::ethabi::Token;
 
+// Function to unwrap a Tuple Token into a vector of Tokens.
 pub fn unwrap_tuple(token: Token) -> Vec<Token> {
+    // Checking if the provided Token is a Tuple.
     if let Token::Tuple(tokens) = token {
+        // If it's a Tuple, return its inner tokens.
         tokens
     } else {
+        // If it's not a Tuple, panic and display the unexpected token.
         panic!("Tuple was expected, got: {}", token);
     }
 }
