59baca20be94e87268051aa7409b4e83adc735ac
ömer burak <116202251+Burakowsky@users.noreply.github.com>
2023-12-31 22:39:48 +0300

    This Rust code provides utilities for commitment calculation related to events queues and bootloader initial content. The events_queue_commitment function calculates the commitment for an events queue, while the bootloader_initial_content_commitment function calculates the commitment for the initial content of a bootloader. Both functions consider whether the context is pre-Boojum or not before performing the commitment calculation.

--- core/lib/commitment_utils/src/lib.rs
+++ core/lib/commitment_utils/src/lib.rs
@@ -1,23 +1,22 @@
 //! Utils for commitment calculation.
+
+// Importing necessary modules and items.
 use zkevm_test_harness::witness::utils::{
     events_queue_commitment_fixed, initial_heap_content_commitment_fixed,
 };
 use zksync_types::{LogQuery, H256, U256, USED_BOOTLOADER_MEMORY_BYTES};
 use zksync_utils::expand_memory_contents;
 
+// Calculate commitment for events queue.
 pub fn events_queue_commitment(events_queue: &Vec<LogQuery>, is_pre_boojum: bool) -> Option<H256> {
+    // Calculate the commitment only if it's not a pre-Boojum context.
     (!is_pre_boojum).then(|| H256(events_queue_commitment_fixed(events_queue)))
 }
 
+// Calculate commitment for bootloader's initial content.
 pub fn bootloader_initial_content_commitment(
     initial_bootloader_contents: &[(usize, U256)],
     is_pre_boojum: bool,
 ) -> Option<H256> {
-    (!is_pre_boojum).then(|| {
-        let full_bootloader_memory =
-            expand_memory_contents(initial_bootloader_contents, USED_BOOTLOADER_MEMORY_BYTES);
-        H256(initial_heap_content_commitment_fixed(
-            &full_bootloader_memory,
-        ))
-    })
-}
+    // Calculate the commitment only if it's not a pre-
+
